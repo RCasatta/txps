4c66e78011007974f530d92c96d7c33c5fd98cd0
Riccardo Casatta <riccardo@casatta.it>
2017-11-16 12:45:15 +0100

    allowing custom shell parameter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rocksdb;
 extern crate rand;
 
+use std::env;
 use rocksdb::DB;
 use rand::Rng;
 use std::time::{Instant , Duration};
@@ -8,31 +9,51 @@ use std::time::{Instant , Duration};
 const  N: usize = 1_000_000;
 const KEY_LENGTH: usize = 20;
 
+
 fn main() {
 
-    println!("Working with {} elements with key length {} bytes", N, KEY_LENGTH );
+    let default_dir = String::from("/tmp/storage");
+    let dir = match env::args().nth(1) {
+        Some(o) => o.parse::<String>().unwrap_or(default_dir),
+        None => default_dir,
+    };
+
+    let n = match env::args().nth(2) {
+        Some(o) => o.parse::<usize>().unwrap_or(N) ,
+        None => N,
+    };
+
+    let mut key_length = match env::args().nth(3) {
+        Some(o) => o.parse::<usize>().unwrap_or(KEY_LENGTH),
+        None => KEY_LENGTH,
+    };
+    if key_length>32 {
+        key_length=32;
+    }
+
+    println!("DB: {} Working with {} elements with key length {} bytes", dir, n, key_length);
 
     let mut vec = Vec::new();
-    let i = Instant::now();
-    for i in 0..N {
-        let r = random20();
-        //println!("{:?}", r);
-        vec.push(r);
+    let start = Instant::now();
+    for _ in 0..n {
+        let r = random();
+        let mut data = Vec::from(&r[..key_length]);
+        vec.push(data);
     }
-    elapsed("Init vector",i.elapsed());
+    elapsed("Init vector", start.elapsed());
 
 
-    let db = DB::open_default("/tmp/storage").unwrap();
+    let db = DB::open_default(dir).unwrap();
     let mut rng = rand::thread_rng();
-    let i = Instant::now();
+    let start = Instant::now();
     let dummy = [0u8;4];
-    for i in 0..N {
+    for i in 0..n {
         db.put(&vec[i],&dummy);
     }
-    elapsed("Random writes",i.elapsed());
+    elapsed("Random writes", start.elapsed());
 
-    let i = Instant::now();
-    for i in 0..N {
+    let start = Instant::now();
+    for i in 0..n {
         match db.get(&vec[i]) {
             Ok(Some(value)) => (),
             Ok(None) => println!("value not found"),
@@ -40,7 +61,7 @@ fn main() {
         }
 
     }
-    elapsed("Random reads",i.elapsed());
+    elapsed("Random reads", start.elapsed());
 }
 
 fn elapsed(title : &str, dur :Duration ) {
@@ -48,10 +69,10 @@ fn elapsed(title : &str, dur :Duration ) {
     println!("{} {:6.3} s {:6.2} tx/s",title, secs, N as f64 / secs);
 }
 
-fn random20() -> [u8;20] {
-    let mut bytes = [0u8;20];
+fn random() -> [u8;32] {
+    let mut bytes = [0u8;32];
     let mut rng = rand::thread_rng();
-    for i in 0..20 {
+    for i in 0..32 {
         bytes[i] = rng.gen::<u8>();
     }
     bytes
